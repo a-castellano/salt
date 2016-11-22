81ffe3defa6a229222cb27c7af4d6383e0f7a180
Álvaro Castellano Vela <acastellano@onestic.com>
2016-11-22 15:03:20 +0100

    added saltmaster config

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -8,6 +8,8 @@ then
         apt-get install -y salt-master
         echo "127.0.0.1   saltmaster" >> /etc/hosts
         git clone https://github.com/a-castellano/salt.git /srv/salt
+        cp /vagrant/files/master /etc/salt/master
+        service salt-master restart
 else
         apt-get install -y salt-minion
         echo "192.168.50.10   saltmaster" >> /etc/hosts
